42fcd6e4d29a45508f325ab175bbef0ce846a972
Shaan-Bot-Project <shaankhank112@gmail.com>
2026-06-28 09:27:34 +0500

    Update 0_SHAAN-GIRL-STATUS.js

--- models/commands/0_SHAAN-GIRL-STATUS.js
+++ models/commands/0_SHAAN-GIRL-STATUS.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["♥️𝑺𝑯𝑨𝑨𝑵-𝑲𝑯𝑨𝑵♥️"];
+   var hi = ["★𝐌𝐈𝐑 𝐇𝐀𝐍𝐄𝐄𝐅★"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
